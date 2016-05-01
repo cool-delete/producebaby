0abaffe3caebdbe016cf6b021f9c108f679d0085
cool-delete <kk520149492@gmail.com>
2016-05-01 23:26:48 +0800

    修改包名版

--- app/src/main/java/cn/studyjams/s1/sj50/dengguandong/MainActivity.java
+++ app/src/main/java/cn/studyjams/s1/sj50/dengguandong/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         init();
 
-
+//.
     }
 
     private void init() {
